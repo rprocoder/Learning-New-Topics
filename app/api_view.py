3d1819f079040e1614903e0f9c226c178599df5f
rprocoder <rprocoder@outlook.com>
2024-02-22 21:19:28 +0530

    Add serializer here

--- app/api_view.py
+++ app/api_view.py
@@ -2,11 +2,38 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializer import UserSerializer
+from rest_framework.generics import CreateAPIView
+from .models import *
+import logging
 
-class UserRegistrationAPIView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
+logger = logging.getLogger(__name__)
+
+###  MEthod : 1
+# class UserRegistrationAPIView(APIView):
+#     print('api',APIView)
+#     def post(self, request):
+#         print('request',request)
+#         serializer = UserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             user = serializer.save()
+#             print('user',user,type(user))
+#             return Response({"userserlizerdaa":serializer.data,'user':str(user)}, status=status.HTTP_201_CREATED)
+#         else:
+#             print("Serializer Errors:", serializer.errors)  # Print out the errors
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#### Method : 2        
+class UserRegistrationAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            user = serializer.save()
-            return Response({"user": serializer.data}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            self.perform_create(serializer)
+            user = serializer.instance
+            logger.debug('User: %s', user)
+            return Response({"userserlizerdaa": serializer.data, 'user': str(user)}, status=status.HTTP_201_CREATED)
+        else:
+            logger.error('Serializer Errors: %s', serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- app/serializer.py
+++ app/serializer.py
@@ -6,6 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     def create(self, validated_data):
+        print('serializer Data',validated_data)
         user = User.objects.create_user(**validated_data)
         return user
 
